2bf999612f33b3478a74734e6ea1204eb460c811
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-05-28 23:35:45 -0400

    fix: add minor fixes

--- src/utils.ts
+++ src/utils.ts
@@ -99,4 +99,17 @@ export function isAsync(fn: CallableFunction): boolean {
   );
 }
 
+export function addS(modelClass: object): string {
+  const word = modelClass.constructor.name.toLowerCase();
+  const exceptions = ['s', 'x', 'z', 'ch', 'sh'];
+
+  if (exceptions.some(ex => word.endsWith(ex))) {
+    return word + 'es';
+  } else if (word.endsWith('y') && !['a', 'e', 'i', 'o', 'u'].includes(word[word.length - 2])) {
+    return word.slice(0, -1) + 'ies';
+  } else {
+    return word + 's';
+  }
+}
+
 export * as Utils from './utils';
