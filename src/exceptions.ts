701d3b237d4bfccde3173660890c686947995864
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-07-03 12:02:47 -0400

    fix: remove unused discovery functionality

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -5,13 +5,6 @@ export class DecoratorError extends Error {
   }
 }
 
-export class DiscoveryError extends Error {
-  constructor(message: string, options?: { cause?: unknown }) {
-    super(message, options);
-    this.name = 'DiscoveryError';
-  }
-}
-
 export class TelegramClientError extends Error {
   constructor(message: string, options?: { cause?: unknown }) {
     super(message, options);
